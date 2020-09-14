9917e54e15bb6edf23e2a30bec223a91149fc683
Jacob Kagon <jacob@kagon.net>
2020-09-14 16:24:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,36 @@
 # Write your methods here
+def loop_message_five_times(message)
+  puts message 
+  puts message
+  puts message
+  puts message
+  puts message
+end 
+
+loop_message_five_times("Hello World.")
+
+def loop_message_n_times(message, integer)
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+loop_message_n_times("Hello Red Balloon.", 10)
+
+
+
+def output_array(array)
+  
+  puts array[counter]
+  counter += 1
+end
+
+output_array(array)
+  
